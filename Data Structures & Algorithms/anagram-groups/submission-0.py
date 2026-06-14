ffb64c8759e580d19369d4852392dc4941fd6e9b
Eshwar <143584428+Eshwar954@users.noreply.github.com>
2026-06-14 13:46:12 +0530

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.py
@@ -0,0 +1,10 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        group=defaultdict(list)
+        for word in strs:
+            sorted_word="".join(sorted(word))
+            group[sorted_word].append(word)
+
+        return list(group.values())
+
+        
